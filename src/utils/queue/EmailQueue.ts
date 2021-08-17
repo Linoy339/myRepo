7c49702a259314c690aa5b89b01b17165a35c534
Linoy <linoy.gopal@zcocorporation.com>
2021-08-17 13:17:58 +0530

    #fixes  consent email

--- src/utils/queue/EmailQueue.ts
+++ src/utils/queue/EmailQueue.ts
@@ -1,6 +1,7 @@
 import Bull from "bull"
-// import nodemailer from "nodemailer"
-import { CredentialRepository} from "../../repository"
+import sgMail from "@sendgrid/mail"
+import { CredentialRepository } from "../../repository/CredentialRepository"
+import { ConsentRepository } from "../../repository/ConsentRepository" 
 import { Mutex } from "async-mutex"
 const clientLock = new Mutex()
 
@@ -9,43 +10,45 @@ export const EmailQueue = new Bull("Email", process.env.REDIS_HOST ?? "")
 
 EmailQueue.process(async (job: any, done: any) => {
   //locking the thread
-  // const release = await clientLock.acquire()
+  const release = await clientLock.acquire()
 
   try {
-  
-  //   if(job.data.isVerified!==undefined) {
-  //       if(job.data.isVerified) {
-  //           //set up credentials
-  //           delete job.data.isVerified                        
-  //           await CredentialRepository._insert(job.data.origin,job.data)
-  //           //send email to the participant
-  //           const transporter = nodemailer.createTransport({
-  //               host: "smtp.mailtrap.io",
-  //               port: 2525,
-  //               auth: {
-  //                 user: "8cdd402c4a3f6f",
-  //                 pass: "6c895aedc6f46d"
-  //               }
-  //             });
+    sgMail.setApiKey(process.env.SENDGRID_API_KEY??'')
+    if(job.data.isVerified!==undefined) {
+        if(job.data.isVerified) {
+            //set up credentials
+            delete job.data.isVerified                        
+            await CredentialRepository._insert(job.data.origin,job.data)
+            let consent = await ConsentRepository._select(job.data.origin)
+            console.log("consent-details",consent)
+            if(!!consent[0].email) {
+            //send email to the participant
+            const msg:any = {
+              to: consent[0].email, // Change to your recipient
+              from: process.env.SENDGRID_API_FROM_ADDR, // Change to your verified sender
+              subject: 'DiiG Consent Signup Success',
+              text: 'DiiG',              
+              html: `<p>Hi ${consent[0].first_name} ${consent[0].last_name},</p><p>You have been successfully registered into Diig app. Please find the credentials below:</p><br><p>Username:${job.data.origin}@lamp.com</p><p>Password:${job.data.origin}</p><br><br> <p>Regards,</p><p>walter.sim@unityhealth.to,</p><p>unityhealth, Canada</p>`,
+            }
            
-  // // send mail with defined transport object
-  // let info = await transporter.sendMail({
-  //   from: '"Diig admin" <admin@diig.com>', // sender address
-  //   to: "linock007@example.com, baz@example.com", // list of receivers
-  //   subject: "DiiG:Account Created", // Subject line
-  //   text: "Hello world?", // plain text body
-  //   html: "<b>Hello world?</b>", // html body
-  // });
-
-  // console.log("Message sent: %s", info.messageId);    
-  //       }
-  //     }
-    // release()
+sgMail
+  .send(msg)
+  .then(() => {
+    console.log('Email sent')
+  })
+  .catch((error) => {
+    console.error(error)
+  })
+  
+}
+        }
+      }
+    release()
    
   } catch (error) {
     //release the lock for thread
-    // release()
-    // console.log(`released job on exception- ${job.data.activity_id}`)
+    release()
+    console.log(`released job on exception- ${job.data.activity_id}`)
   }
   done()
 
